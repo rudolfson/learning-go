a355bfa402079270e8da7b55af12c23bd0425348
Lars Heller <lars.heller@ibs.valantic.com>
2019-01-08 14:39:30 +0100

    flowcontrol 1-8

--- a-tour-of-go/B_flowcontrol/08_sqrt.go
+++ a-tour-of-go/B_flowcontrol/08_sqrt.go
@@ -6,10 +6,13 @@ import (
 )
 
 const ITERATIONS = 10
+const DIFF = 0.00000001
 
 func Sqrt(x float64) float64 {
-    var z float64 = 1.0
-    for i := 0; i < ITERATIONS; i++ {
+    z := 1.0
+    prev := z - 10*DIFF
+    for i := 0; i < ITERATIONS && math.Abs(z - prev) > DIFF; i++ {
+        prev = z
         z -= (z*z - x) / (2*z)
         fmt.Println("z is", z)
     }
@@ -17,7 +20,20 @@ func Sqrt(x float64) float64 {
 }
 
 func main() {
-    var x = 3.0
+    var x = 2.0
+    fmt.Printf("sqrt(%g)\n", x)
+    fmt.Println(Sqrt(x))
+    fmt.Println(math.Sqrt(x))
+    x = 3.0
+    fmt.Printf("sqrt(%g)\n", x)
+    fmt.Println(Sqrt(x))
+    fmt.Println(math.Sqrt(x))
+    x = 300.0
+    fmt.Printf("sqrt(%g)\n", x)
+    fmt.Println(Sqrt(x))
+    fmt.Println(math.Sqrt(x))
+    x = 303240.0
+    fmt.Printf("sqrt(%g)\n", x)
     fmt.Println(Sqrt(x))
     fmt.Println(math.Sqrt(x))
 }
